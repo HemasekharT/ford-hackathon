d856a9498b8fcee4a6bcbc43d9dbedcd5a5cbcd4
sumo <sumo@MGC15Y604KJ1GP.fbpld77.ford.com>
2019-06-25 12:09:42 -0400

    first commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const techCompanies = [
-  { label: "Apple", value: 1 },
-  { label: "Facebook", value: 2 },
-  { label: "Netflix", value: 3 },
-  { label: "Tesla", value: 4 },
-  { label: "Amazon", value: 5 },
-  { label: "Alphabet", value: 6 },
+const fordCars = [
+  { label: "Lincoln", value: 1 },
+  { label: "Mustang", value: 2 },
+  { label: "Ford Truck", value: 3 }
 ];
+
+const fetures = [
+  {
+  }
+]
 
 const App = () => (
     <div className="container">
       <div className="row">
         <div className="col-md-4"></div>
         <div className="col-md-4">
-          <Select options={ techCompanies } />
+          <Select options={ fordCars } />
         </div>
         <div className="col-md-4"></div>
       </div>
